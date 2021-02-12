537279ec29355df40a25ee0836bfeb17a3ba3216
Nicu20 <leontenicu01@gmail.com>
2021-02-12 18:12:58 +0200

    fixing bugs

---
+++ js/main.js
@@ -0,0 +1,42 @@
+//let ;
+//name="Ion"; //string
+
+
+//let age=20; //variabila numerica
+let isMarried=false; //variabila boolean
+let a=5;
+let b=5;
+//console.log(a+b); 
+
+
+let hobby=['pescuit', 'citit', 'sport']; //variabila de tip array
+let examene=[2010, 2011, 2012];
+
+
+//console.log('numele este ' + name);
+//console.log('varsta este ' + age);
+//console.log('e casatorit ' + isMarried);
+//console.log('hobbi-urile sunt ' + hobby);
+//console.log('al dilea hobby este ' + hobby[1]);
+
+let car = { //object
+    brand: 'Audi',
+    production: 2010,
+    extraOptions: ['aer conditionat', 'servo directie'],
+} 
+//console.log(car);
+
+let name= 'Mihai';
+let age= 34;
+let stareCivila= true;
+let culoriPreferate= ['verde', 'albastru', 'galben'];
+
+console.log('Sunt ' + name, 'si am varsta de ' + age, 'dar sunt stare civila ' + stareCivila);
+console.log('A doua culoare preferata este ' + culoriPreferate[2]);
+
+let animal={
+    tip: 'caine',
+    greutate: 10,
+    nume: ['Bruno', 'Jack', 'Igor']
+}
+console.log(animal);
